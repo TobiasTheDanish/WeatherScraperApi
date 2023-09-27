d9150053367ba19804b303bfa82cf13fdb4e7ad6
TobiasTheDanish <t.hc199811@gmail.com>
2023-09-27 10:38:45 +0200

    Function for checking if weather is persisted

--- src/main/java/dat/sem3/Main.java
+++ src/main/java/dat/sem3/Main.java
@@ -1,26 +1,36 @@
 package dat.sem3;
 
-import dat.sem3.webscraping.WeatherApiReader;
+import dat.sem3.dao.WeatherDAO;
+import dat.sem3.dto.WeatherDTO;
+import dat.sem3.model.WeatherEntity;
+import dat.sem3.webscraping.WeatherScraper;
+
+import java.io.IOException;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        WeatherApiReader.getCurrentWeather();
         /*
+        WeatherApiReader.getCurrentWeather();
+         */
+
         try {
 
             List<WeatherDTO> list = WeatherScraper.getInstance().scrape();
-            //WeatherApiReader.ApiReader();
 
             list.forEach(weatherDTO -> {
                 WeatherDAO dao = WeatherDAO.getInstance();
-                WeatherEntity entity = dao.createWeatherEntity(weatherDTO.convertToEntity());
-                //System.out.println("Entity id: " + entity.getId());
+                WeatherEntity entity = weatherDTO.convertToEntity();
+                if (dao.isWeatherAlreadyPersisted(entity)) {
+                    dao.updateWeatherEntity(entity);
+                } else {
+                    dao.createWeatherEntity(entity);
+                }
             });
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
         System.exit(0);
 
-         */
     }
 }
